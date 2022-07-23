24120d02a700b8145253736ebea0f4daa38ff881
Fuzzballs <105466886+Fuzzballs@users.noreply.github.com>
2022-07-22 19:51:12 -0500

    Create enterer.py

---
+++ enterer.py
@@ -0,0 +1,20 @@
+from ast import Return
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+#Gives the entry bar
+e = Entry(root, width=50)
+e.pack()
+
+root.geometry("750x250")
+#This is what will be printed on screen
+def callback():
+    Label(root, text=e.get(), font=('Georgia 20 bold')).pack()
+#What the button will do
+btn = ttk.Button(root, text="Show the thing", command=callback)
+btn.pack(ipadx=10)
+#What key will be bound to the action
+root.bind('<Return>',lambda event:callback())
+
+root.mainloop()
